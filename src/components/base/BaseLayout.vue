054aa49e65ba717bef0ef65927409b27e8a36454
anadamasio <2051320@student.uma.pt>
2021-11-28 02:10:52 +0000

    fiquei no minuto 50:17

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <ion-page>
+
+        <ion-header>
+            <ion-toolbar>
+
+                <ion-buttons>
+                    <ion-back-button :default-href="pageDefaultBackLink"></ion-back-button>
+                </ion-buttons>
+
+                <ion-title>{{ pageTitle }}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+            <ion-list>
+                <slot />
+            </ion-list>
+        </ion-content>
+
+    </ion-page>
+
+</template>
+
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons } from '@ionic/vue';
+
+export default {
+    props: ['pageTitle', 'pageDefaultBackLink'],
+
+    components: {
+        IonPage: IonPage,
+        IonHeader: IonHeader,
+        IonToolbar: IonToolbar,
+        IonTitle: IonTitle,
+        IonContent: IonContent,
+        IonBackButton: IonBackButton,
+        IonButtons: IonButtons
+    }    
+}
+</script>
---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <base-layout page-title = "All Memories">
+
+        <ion-item router-link="/memories/1">A trip into the mountains</ion-item>
+        <ion-item>Surfing the sea side</ion-item>
+        <ion-item>Good eating</ion-item>
+
+    </base-layout>
+
+</template>
+
+
+<script>
+import { IonItem } from '@ionic/vue';
+
+export default {
+    components: {
+        IonItem: IonItem
+    },
+
+    data() {
+        return {
+            memories: [
+                { 
+                    id: 'm1', 
+                    title: 'A trip into the mountains',
+                    description: 'It was a really nice trip'
+                },
+                {
+                    id: 'm2',
+                    title: 'Surfing the sea side',
+                    description: 'Feeling the cool breeze'
+                },
+                {
+                    id: 'm3',
+                    title: 'Good eating',
+                    description: 'Really tasty!'
+                }
+            ]
+        }
+    }
+}
+</script>
