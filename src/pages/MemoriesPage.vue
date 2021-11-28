481bfa4522000ac722ecd75a236b6dc7d3295345
anadamasio <2051320@student.uma.pt>
2021-11-28 14:25:23 +0000

    fiquei no minuto 58:47

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -2,7 +2,17 @@
 
     <base-layout page-title = "All Memories">
 
-        <ion-item router-link="/memories/1">A trip into the mountains</ion-item>
+        <ion-item v-for = "memory in memories" :router-link="`/memories/${memory.id}`" :key="memory.id">
+
+            <ion-thumbnail>
+                <ion-img :src="memory.image" alt="memory.title"></ion-img>
+            </ion-thumbnail>
+
+            <ion-label>
+                {{ memory.title }}
+            </ion-label>
+
+        </ion-item>
 
     </base-layout>
 
@@ -10,35 +20,18 @@
 
 
 <script>
-import { IonItem } from '@ionic/vue';
+import { IonItem, IonImg, IonThumbnail, IonLabel } from '@ionic/vue';
 
 export default {
     components: {
-        IonItem: IonItem
+        IonItem: IonItem,
+        IonImg: IonImg,
+        IonThumbnail: IonThumbnail,
+        IonLabel: IonLabel
     },
 
     data() {
         return {
-            memories: [
-                { 
-                    id: 'm1', 
-                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Mighty_Mountains_with_Snow.jpg/640px-Mighty_Mountains_with_Snow.jpg",
-                    title: 'A trip into the mountains',
-                    description: 'It was a really nice trip'
-                },
-                {
-                    id: 'm2',
-                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg/640px-Paracas_National_Reserve%2C_Ica%2C_Peru-3April2011.jpg",
-                    title: 'Surfing the sea side',
-                    description: 'Feeling the cool breeze'
-                },
-                {
-                    id: 'm3',
-                    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/640px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
-                    title: 'Good eating',
-                    description: 'Really tasty!'
-                }
-            ]
         }
     }
 }
